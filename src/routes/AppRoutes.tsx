2cb87413aeab3423f01d3f65d5b8bdc4a7ef9cca
Vencislav Venkov <wenci_97_08@abv.bg>
2025-03-12 13:49:32 +0200

    Import assets.

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,14 +1,14 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
+import Home from "../pages/Home";
+import About from "../pages/About";
 
-function App() {
+const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
     </Routes>
   );
-}
+};
 
-export default App;
+export default AppRoutes;
